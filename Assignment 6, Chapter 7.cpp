9ce08a0ce20a771b5f5d7ff188fd08ce85b07e23
SanguineBird <SanguineBird@users.noreply.github.com>
2014-10-11 13:20:11 -0400

    Update Assignment 6, Chapter 7.cpp
    The test function now does things!

--- Assignment 6, Chapter 7.cpp
+++ Assignment 6, Chapter 7.cpp
@@ -11,15 +11,31 @@
 int mystery( const char *s1, const char *s2 );
 
 int main(void){
-  bool found = false;
-  char array[] = "This is a string!";
+  char array1[] = "This is a string!";
   char array2[] = "And here is another.";
+  char string1[] = "This is a string!";
+  char string2[] = "And here is another.";
   int i = 0;
+  int *compare;
   
-  /*While(! mystery()){
-    printf("%d", *array[i]);
-    i++;
-  }*/
+  printf("Compare array1: %s\n to array2: %s", array1, array2);
+  compare = mystery(array1, array2);
+  If(*compare == 0){
+    printf("The strings are identical.");
+  }
+  Else If(*compare == 1){
+    printf("The strings are not identical.");
+  }
+  
+
+  printf("Compare array1: %s\n to string1: %s", array1, string1);
+  compare = mystery(array1, string1);
+  If(*compare == 0){
+    printf("The strings are identical.");
+  }
+  Else If(*compare == 1){
+    printf("The strings are not identical.");
+  }
   
   system("pause"); return 0;
 }
@@ -38,8 +54,8 @@ int mystery( const char *s1, const char *s2 )
 /*The mystery function takes two arguments as costant points of the char type.
 The for loop in this function has no initialization statement.
 The for loop will run as long as neither *s1 nor *s2 equal the null character which indicates the end of a string.
-Each time the for loop iterates, both points are incremented to the next location in anarray.
-The if statment ensures that *s1 and *s2 do not reference the same location in the same array.
+Each time the for loop iterates, both points are incremented to the next location in an array.
+The if statment ensures that *s1 and *s2 do not reference the same value.
 The function returns zero if this condition is met.
 When the loop is completed, the function returns one.
-This function compares pointers which reference two locations in the same array. It returns the value 0 until it finds the end of the string. Then, it returns the value 1. These return values probably control a boolean variable.*/
+This function compares pointers which reference two array locations. It returns the value 0 until it finds the end of the string. Then, it returns the value 1.*/
